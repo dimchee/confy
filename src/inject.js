9c2edacecc084b575b20b1bd75d5cdc7173a61bd
dimchee <dimchee90@gmail.com>
2024-08-01 17:17:20 +0200

    Change to CBC

--- src/inject.js
+++ src/inject.js
@@ -1,7 +1,8 @@
 function aes_from_key(key) {
   if (key == null) key = "";
+  const iv = [ 21, 13, 23, 24, 111, 26, 27, 13, 29, 30, 31, 28, 33, 201,35, 36 ];
   key = aesjs.utils.utf8.toBytes(key.padEnd(32, "0"))
-  return new aesjs.ModeOfOperation.ctr(key)
+  return new aesjs.ModeOfOperation.cbc(key, iv)
 }
 
 const key = new URLSearchParams(window.location.search).get("key")
