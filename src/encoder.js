fbc894cb666d904b543c2495a646af53613ec50c
dimchee <dimchee90@gmail.com>
2024-08-01 17:19:02 +0200

    Back to CTR

--- src/encoder.js
+++ src/encoder.js
@@ -6,19 +6,20 @@ const fs = require('node:fs');
 var [key, b] = process.argv.slice(2)
 console.log (`key: ${key} b: ${b}`)
 
+
 function aes_from_key(key) {
   if (key == null) key = "";
-  const iv = [ 21, 13, 23, 24, 111, 26, 27, 13, 29, 30, 31, 28, 33, 201,35, 36 ];
   key = aesjs.utils.utf8.toBytes(key.padEnd(32, "0"))
-  return new aesjs.ModeOfOperation.cbc(key, iv)
+  return new aesjs.ModeOfOperation.ctr(key)
 }
 
 JSDOM.fromFile('examples/example.html').then(dom => {
   var document = dom.window.document;
   Array.from(document.getElementsByClassName("secret")).forEach(x => {
-    pad = Math.floor((x.innerHTML.length - 1) / 16 + 1) * 16
-    var textBytes = aesjs.utils.utf8.toBytes( x.innerHTML.padEnd(pad, ' '));
-    var encrypted = aesjs.utils.hex.fromBytes(aes_from_key(key).encrypt(textBytes));
+    var textBytes = aesjs.utils.utf8.toBytes(x.innerHTML)
+    var encrypted = aesjs.utils.hex.fromBytes(
+        aes_from_key(key)?.encrypt(textBytes)
+    );
     console.log(encrypted)
     x.innerHTML = encrypted
   });
