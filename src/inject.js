1182e69ea2f32f93961d74c75ac8713f6ee404f5
andrijast <andrija.stevanovic56@gmail.com>
2024-08-11 07:28:13 +0200

    Include nonce in CTR mode for increased security

--- src/inject.js
+++ src/inject.js
@@ -1,13 +1,16 @@
-function aes_from_key(key) {
-  if (key == null) key = "";
-  key = aesjs.utils.utf8.toBytes(key.padEnd(32, "0"))
-  return new aesjs.ModeOfOperation.ctr(key)
+function aes_from_key(key, nonce) {
+    if (key == null) key = "";
+    key = aesjs.utils.utf8.toBytes(key.padEnd(32, "\0"))
+    return new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(nonce))
 }
 
 const key = new URLSearchParams(window.location.search).get("key")
 
 Array.from(document.getElementsByClassName('secret')).forEach(x => {
+    const content = x.innerHTML.trim();
+    const nonce = aesjs.utils.hex.toBytes(content.slice(0, 32));
+    const ciphertext = aesjs.utils.hex.toBytes(content.slice(32));
     x.innerHTML = aesjs.utils.utf8.fromBytes(
-        aes_from_key(key).decrypt(aesjs.utils.hex.toBytes(x.innerHTML.trim()))
+        aes_from_key(key, nonce).decrypt(ciphertext)
     );
 })
